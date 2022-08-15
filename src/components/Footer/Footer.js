b478931846eba79910566e1bb866181b1a993ae6
iamparul <himanshu@toothsi.in>
2022-08-15 16:54:13 +0100

    new commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <a
+      href="https://github.com/sainparul19/portfolio02"
+      className="link footer__link"
+    >
+      Created By Parul Sain
+    </a>
+  </footer>
+);
+
+export default Footer;
---
+++ src/portfolio.js
@@ -0,0 +1,51 @@
+const header = {
+  homepage: "https://sainparul19.github.com/portfolio02",
+  title: "PS.",
+};
+
+const about = {
+  name: "Parul Sain",
+  role: "Front End Engineer",
+  description:
+    "I always love to explore new things and eager to learn and experiment. For me, a website is all about experience. New technology excites me and motivates me to explore more . ",
+  resume: "https://example.com",
+  social: {
+    linkedin: "https://linkedin.com",
+    github: "https://github.com",
+  },
+};
+
+const projects = [
+  {
+    name: "Project 1",
+    description: "",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+  {
+    name: "Project 2",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+  {
+    name: "Project 3",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+];
+
+const skills = ["HTML 5", "CSS 3", "JavaScript", "React JS", "Next JS"];
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: "parul.sain019@gmail.com.com",
+};
+
+export { header, about, projects, skills, contact };
