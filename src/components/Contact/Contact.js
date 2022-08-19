4d7aad85f2b2408a8ada491cba75d186d3cd68f5
iamparul <himanshu@toothsi.in>
2022-08-20 00:53:46 +0100

    new changes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,54 @@
-import { contact } from "../../portfolio";
 import "./Contact.css";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  if (!contact.email) return null;
+  function sendEmail(e) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_mu8vuud",
+        "template_ckj8zon",
+        e.target,
+        "TSxKF5rMSfHzYGmJp"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
 
   return (
-    <section className="section contact center" id="contact">
-      <h2 className="section__title">Contact</h2>
-      <a href={`mailto:${contact.email}`}>
-        <span type="button" className="btn btn--outline">
-          Email me
-        </span>
-      </a>
-    </section>
+    <div
+      className="container border"
+      style={{
+        marginTop: "50px",
+        width: "90%",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <h2 style={{ marginTop: "25px" }}>Contact Form</h2>
+      <form
+        className="row"
+        style={{ margin: "25px 85px 75px 100px", width: "60%" }}
+        onSubmit={sendEmail}
+      >
+        <label>Name</label>
+        <input type="text" name="name" className="form-control" />
+
+        <label>Email</label>
+        <input type="email" name="user_email" className="form-control" />
+
+        <label>Message</label>
+        <textarea name="message" rows="4" className="form-control" />
+        <input
+          type="submit"
+          value="Send"
+          className="form-control btn btn-primary"
+          style={{ marginTop: "30px" }}
+        />
+      </form>
+    </div>
   );
 };
 
--- src/portfolio.js
+++ src/portfolio.js
@@ -8,7 +8,8 @@ const about = {
   role: "Front End Engineer",
   description:
     "I always love to explore new things and eager to learn and experiment. For me, a website is all about experience. New technology excites me and motivates me to explore more . ",
-  resume: "https://example.com",
+  resume:
+    "https://drive.google.com/file/d/1HNClECeBO68YjdNJgzbyOhqOdYRRCCLy/view?usp=sharing",
   social: {
     linkedin: "https://linkedin.com",
     github: "https://github.com",
@@ -17,34 +18,39 @@ const about = {
 
 const projects = [
   {
-    name: "Project 1",
-    description: "",
-    stack: ["SASS", "TypeScript", "React"],
+    name: "Toothsi",
+    description: "Created toothsi UI clone from scratch with React JS .",
+    stack: [],
     sourceCode: "https://github.com",
-    livePreview: "https://github.com",
+    livePreview: "https://toothsi-clone.web.app",
   },
   {
-    name: "Project 2",
-    description:
-      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
-    stack: ["SASS", "TypeScript", "React"],
+    name: "Instagram",
+    description: "Build instagram clone with React JS and Material UI",
+    stack: [],
     sourceCode: "https://github.com",
-    livePreview: "https://github.com",
+    livePreview: "https://instagram-clone-33a1b.web.app",
   },
   {
-    name: "Project 3",
-    description:
-      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
-    stack: ["SASS", "TypeScript", "React"],
+    name: "Facebook",
+    description: "Build facebook clone with React JS and Material UI",
+    stack: [],
     sourceCode: "https://github.com",
-    livePreview: "https://github.com",
+    livePreview: "https://fb-clone-3a0b7.web.app",
+  },
+  {
+    name: "CRED",
+    description:
+      "Created clone of cred web app from scratch with React JS by making reusable components",
+    stack: [],
+    sourceCode: "https://github.com",
+    livePreview: "https://cred-clone-41b93.web.app",
   },
 ];
 
 const skills = ["HTML 5", "CSS 3", "JavaScript", "React JS", "Next JS"];
 
 const contact = {
-  // email is optional - if left empty Contact section won't show up
   email: "parul.sain019@gmail.com.com",
 };
 
